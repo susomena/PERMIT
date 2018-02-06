e099b0807bc2ae524971c3d53d17e160f88c79c8
jmena <jmena@umh.es>
2018-02-06 10:59:16 +0100

    Added the main source file of the simulator. Simulation parameters and execution.

---
+++ simulator.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 Jesús Mena-Oreja <jmena@umh.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+
+import os
+import sys
+import argparse
+import ccparams as cc
+from utils import set_par, change_lane, communicate, get_distance, get_par, start_sumo, running
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("Please, declare environment variable 'SUMO_HOME'")
+
+import sumolib
+import traci
+
+parser = argparse.ArgumentParser(description="Platooning simulator for adding autonomous mobility on SUMO scenarios")
+arg_group = parser.add_argument_group('Simulator parameters')
+arg_group.add_argument("-c", "--configuration-file", type=str, help="config file of the SUMO scenario", metavar="FILE",
+                       required=True)
+arg_group.add_argument("-D", "--demo", action="store_true", help="run SUMO indefinitely")
+arg_group.add_argument("-m", "--max-step", default=6000, type=int, help="max simulation step for SUMO")
+args = parser.parse_args()
+
+
+def main():
+    start_sumo(args.configuration_file, False)
+    step = 0
+
+    while running(args.demo_mode, step, args.max_step):
+        traci.simulationStep()
+        step += 1
+
+    traci.close()
+
+
+if __name__ == "__main__":
+    main()
